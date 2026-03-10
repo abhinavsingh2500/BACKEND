d384acab5a23ade7944a3bf0e79c5289c51f6d58
abhinavsingh2500 <abhinavsingh250000@gmail.com>
2026-03-10 13:14:02 +0530

    server creation

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-const fs = require('fs');
-/*fs.writeFile('Abhinav.txt', 'I am Abhinav', function(err) {
-     if(err){
-         console.error(err);
-     }
-     else{
-         console.log('done successfully');
-     }
- });*/
+const http=require('node:http');
+const server=http.createServer( function (req, res) {
+    res.end("Hello World");
+});
 
-
- /*fs.appendFile('Abhinav.txt', ' and I am a curious guy', function(err){
-    if (err){
-        console.error(err);
-    }
-    else{
-        console.log('appended successfully');
-    }
-});*/
-
-// 
-/*fs.unlink('personalinfo.txt',function(err){
-    if(err){
-        console.error(err);
-    }           
-    else{
-        console.log('file deleted successfully');
-    }
-});*/
+server.listen(3000);
