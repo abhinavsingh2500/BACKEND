7f4dec56b7e6ed14da8b5da4b83a7dbaf70571f6
abhinavsingh2500 <abhinavsingh250000@gmail.com>
2026-03-10 12:46:22 +0530

    fs operations

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-var arr=[1,2,3,4,5];
-arr.forEach(function(val){
-    console.log(val + "Hello");
-});
- var newarr=arr.filter(function(val){ 
-    if(val%2==0){
-        return true;
-    }
-    return false;
- })
-    console.log(newarr);
+const fs = require('fs');
+/*fs.writeFile('Abhinav.txt', 'I am Abhinav', function(err) {
+     if(err){
+         console.error(err);
+     }
+     else{
+         console.log('done successfully');
+     }
+ });*/
 
-    var newarr2=arr.map(function(val){
-        return val*2;
-    });
+
+ /*fs.appendFile('Abhinav.txt', ' and I am a curious guy', function(err){
+    if (err){
+        console.error(err);
+    }
+    else{
+        console.log('appended successfully');
+    }
+});*/
+
+fs.rename('Abhinav.txt','personalinfo.txt', function(err){
+    if (err){console.error(err);}
+    else{
+        console.log('renamed successfully');
+    }
+});
