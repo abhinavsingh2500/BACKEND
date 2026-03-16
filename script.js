9835f553ed804bb3c93b86b0f2613a4bc7eb76b4
abhinavsingh2500 <abhinavsingh250000@gmail.com>
2026-03-16 20:13:59 +0530

    error middleware

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ const app=express();
 
 app.get("/", function(req,res){
     res.send("he he he ");
-})
+});
+  
 
 app.get("/profile", function(req,res){
-    res.send("Abhinav'sprofile");
-})
+    res.send("Abhinav's  profile");
+});
+ 
+app.get("/about", (req,res,next)=>{
+    return next(new Error("daal m kuchh kala hai daya"));
+    
+});
+
+app.get("/contact", (req,res)=>{
+    res.send("contact me at abhinav@example.com");
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("2 min ruko abhi thik krke deta hu");
+});
 
 app.listen(3000);
