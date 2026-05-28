319290b4805b6aca2e190330b6105a2bc62220a4
abhinavsingh2500 <abhinavsingh250000@gmail.com>
2026-05-29 02:01:08 +0530

    my notes app

--- notes/index.js
+++ notes/index.js
@@ -1,11 +1,44 @@
 const express=require('express');
 const app=express();
 const path =require('path');
+const fs=require('fs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"..","notes_ka_maal")));
+app.use(express.static(path.join(__dirname,"..",  "notes_ka_maal")));
+
+// Create documents folder if it doesn't exist
+if (!fs.existsSync('./documents')){
+    fs.mkdirSync('./documents');
+}
+
 app.get("/",function(req,res){
-    res.render("notes");
+    fs.readdir('./documents',function(err,files){
+        if(err){
+            res.render("notes",{documents: []});
+        } else {
+            res.render("notes",{documents: files});
+        }
+    });
 });
+app.get('/notes/:noteName', function(req,res){fs.readFile(`./documents/${req.params.noteName}`, 'utf8', function(err, data){
+    if(err){
+        res.send("Note not found"); 
+    } else {
+        res.send(data);
+    }
+});});
+
+app.post("/add-note", function(req,res){
+    let title = req.body.title;
+    let content = req.body.content;
+    fs.writeFile(`./documents/${title}.txt`, content, function(err){
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/");
+    });
+});
+
 app.listen(3000,()=>{console.log("notes  is running on port 3000")});
+
