813173e55bad55630fa6e084a9c30c80fb50aa5a
abhinavsingh2500 <abhinavsingh250000@gmail.com>
2026-06-27 12:10:26 +0530

    bcrypt hashing and comparison

--- AuthnCookie/app.js
+++ AuthnCookie/app.js
@@ -1,13 +1,30 @@
- const express=require('express');
- const cookieParser=require('cookie-parser');
-const app=express();
+ const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const app = express();
+
 app.use(cookieParser());
-app.get('/',(req,res)=>{
-    res.cookie("name","Abhinav")
-    res.send('Cookie set!')
+
+app.get('/', (req, res) => {
+  res.cookie('name', 'Abhinav');
+
+  bcrypt.genSalt(10, (err, salt) => {
+    
+
+    bcrypt.hash('Abhinav', salt, (err, hash) => {
+      if (err) throw err;
+      console.log(hash);
+
+      bcrypt.compare('Abhinav', hash, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+      });
+    });
+  });
+
+  res.send('Cookie set!');
 });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-
 });
